56aceaa8870b8e77f994ed2a6b54b5ab1773d5be
AnkitSoni03 <120096797+AnkitSoni03@users.noreply.github.com>
2025-02-17 23:25:28 +0530

    update

--- 06-currency-converter-custom-hooks/src/App.jsx
+++ 06-currency-converter-custom-hooks/src/App.jsx
@@ -3,6 +3,8 @@ function App() {
   return (
     <>
       srdfguipgfzgoip;t7r6etdfghjo'/jhg,fdnsbzxcgilu;ok/
+      <h1>srdfguipgfzgoip;t7r6etdfghjo'/jhg,fdnsbzxcgilu;ok/</h1>
+      <p5>srdfguipgfzgoip;t7r6etdfghjo'/jhg,fdnsbzxcgilu;ok/</p5>
     </>
   )
 }
